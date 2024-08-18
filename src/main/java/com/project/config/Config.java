19ae30f35ada918b0e492209bc993375a84f3b4f
Vaibhav <sharmavaibhav185@gmail.com>
2024-08-18 22:12:49 +0530

    added DB config and security config

--- src/main/java/com/project/config/Config.java
+++ src/main/java/com/project/config/Config.java
@@ -1,7 +1,10 @@
 package com.project.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,12 +13,18 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.project.service.MyUserDetailService;
 
 @Configuration
 @EnableWebSecurity
 public class Config {
+	
+	@Autowired
+	private MyUserDetailService userDetailsService;
 	
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http ) throws Exception {
@@ -25,6 +34,13 @@ public class Config {
 					.httpBasic(Customizer.withDefaults())
 					.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 					.build();
+	}
+	
+	public AuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+		provider.setUserDetailsService(userDetailsService);
+		return provider;
 	}
 	
 	@Bean
